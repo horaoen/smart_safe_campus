0adc295b720a066ccaa9c630b1171552ef3c9aa4
horaoen <horaoen@gmail.com>
2022-07-18 09:54:53 +0800

    Initial commit

---
+++ src/main/java/com/horaoen/smart_safe_campus/SmartSafeCampusApplication.java
@@ -0,0 +1,13 @@
+package com.horaoen.smart_safe_campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSafeCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSafeCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horaoen/smart_safe_campus/SmartSafeCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horaoen.smart_safe_campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSafeCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
